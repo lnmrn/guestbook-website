6f1d564dca3c6d1da48056b38d2d85f6d7915fba
lnmrn <lnmrn.dev@gmail.com>
2025-11-03 15:48:39 +0100

    Add accomodation/cabins page

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,11 +1,35 @@
+import CabinCard from "./CabinCard";
+
 export const metadata = {
   title: "Cabins",
 };
 
 function Page() {
+  const cabins = [
+    { name: "Cabin001" },
+    { name: "Cabin002" },
+    { name: "Cabin003" },
+  ];
   return (
-    <div>
-      <h1>All Cabins</h1>
+    <div className="mt-10">
+      <h1 className="text-4xl mb-5 text-accent-400 font-medium">
+        Our Luxory Cabins
+      </h1>
+      <p className="text-primary-200 text-lg mb-10">
+        Cozy yet luxurious cabins, located right in the heart of the Italian
+        Dolomites. Imagine waking up to beautiful mountain views, spending your
+        days exploring the dark forests around, or just relaxing in your private
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
+      </p>
+      {cabins?.length > 0 && (
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+          {cabins.map((cabin) => (
+            <CabinCard cabin={cabin} key={cabin.id} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
