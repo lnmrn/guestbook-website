6aae5faa9487e8caeda7b3ae69deb20382163404
lnmrn <lnmrn.dev@gmail.com>
2025-11-04 16:55:54 +0100

    Add /account/profile segment

---
+++ app/_components/SelectCountry.js
@@ -0,0 +1,9 @@
+function SelectCountry(name, id, defaultCountry) {
+  return (
+    <select className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm">
+      <option>Country</option>
+    </select>
+  );
+}
+
+export default SelectCountry;
