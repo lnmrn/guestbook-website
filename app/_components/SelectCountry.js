aad9e09020de95c00be7d52b59b2a278441f35b7
lnmrn <lnmrn.dev@gmail.com>
2025-11-05 15:26:45 +0100

    Update styling and layout in SelectCountry

--- app/_components/SelectCountry.js
+++ app/_components/SelectCountry.js
@@ -1,8 +1,22 @@
-function SelectCountry(name, id, defaultCountry) {
-  //moras cili data-service file prepisati da bi ova komponenta proradila
+import { getCountries } from "../_lib/data-service";
+
+async function SelectCountry(name, id, defaultCountry) {
+  const countries = await getCountries();
+  const flag =
+    countries.find((country) => country.name === defaultCountry)?.flag ?? "";
   return (
-    <select className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm">
-      <option>Country</option>
+    <select
+      name={name}
+      id={id}
+      defaultValue={`${defaultCountry}%${flag}`}
+      className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
+    >
+      <option value="">Select country...</option>
+      {countries.map((country) => (
+        <option key={country.name} value={`${country.name}%${country.flag}`}>
+          {country.name}
+        </option>
+      ))}
     </select>
   );
 }
