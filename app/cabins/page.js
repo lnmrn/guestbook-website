aad2745e1ba64b0a0ee806979d6dd715ed71ae7d
lnmrn <lnmrn.dev@gmail.com>
2025-11-07 17:14:17 +0100

    Update cabins segment with Suspense

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,13 +1,14 @@
+import { Suspense } from "react";
 import CabinCard from "../_components/CabinCard";
+import CabinList from "../_components/CabinList";
 import { getCabins } from "../_lib/data-service";
+import Spinner from "../_components/Spinner";
 
 export const metadata = {
   title: "Cabins",
 };
 
-async function Page() {
-  const cabins = await getCabins();
-  console.log(cabins);
+function Page() {
   return (
     <div className="mt-10">
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -21,13 +22,9 @@ async function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
-      {cabins?.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
--- app/loading.js
+++ app/loading.js
@@ -1,5 +1,7 @@
+import Spinner from "./_components/Spinner";
+
 function Loading() {
-  return <div>LOADING...</div>;
+  return <Spinner />;
 }
 
 export default Loading;
