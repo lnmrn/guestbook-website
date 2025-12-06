83f86d0e7bf753dcb000bcbcc3fceee5d5fb0411
lnmrn <lnmrn.dev@gmail.com>
2025-12-06 16:01:27 +0100

    Refactor [cabinId] by adding ReservationSection.js

---
+++ app/_components/ReservationSection.js
@@ -0,0 +1,13 @@
+import DateSelector from "./DateSelector";
+import ReservationForm from "./ReservationForm";
+
+function ReservationSection() {
+  return (
+    <div className="grid grid-cols-2 border border-primary-800 min-h-[400px] mt-10 mb-5">
+      <DateSelector />
+      <ReservationForm />
+    </div>
+  );
+}
+
+export default ReservationSection;
