6a41a4fd7d79aee6a7ce706a60f9f0f8ae069052
lnmrn <lnmrn.dev@gmail.com>
2026-03-11 17:10:30 +0100

    Made sure the submit booking button is only rendered in if dates are selected

--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -83,10 +83,15 @@ function ReservationForm({ cabin, user }) {
         </div>
 
         <div className="flex justify-end items-center gap-6">
-          <p className="text-primary-300 text-base">Start by selecting dates</p>
-          <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-            {isPending ? <SpinnerMini /> : "Reserve now"}
-          </button>
+          {!(startDate && endDate) ? (
+            <p className="text-primary-300 text-base">
+              Start by selecting dates
+            </p>
+          ) : (
+            <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
+              {isPending ? <SpinnerMini /> : "Reserve now"}
+            </button>
+          )}
         </div>
       </form>
     </div>
