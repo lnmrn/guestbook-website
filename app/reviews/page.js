2de74176315cadd05caeceb1f6bca218d86ee643
lnmrn <lnmrn.dev@gmail.com>
2026-03-12 15:29:48 +0100

    Just spacing between list items

--- app/reviews/page.js
+++ app/reviews/page.js
@@ -4,7 +4,7 @@ import Review from "../_components/Review";
 async function Page() {
   const reviews = await getReviews();
   return (
-    <div className="flex flex-col items-center border-accent-700">
+    <div className="flex flex-col items-center border-accent-700 space-y-10">
       {reviews.map((review) => (
         <Review key={review.id} review={review} />
       ))}
